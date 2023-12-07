b0856ff7526338eb62c7805f45757867d10fbe1d
kenneth <59539715+cloudken123@users.noreply.github.com>
2023-12-07 15:32:23 +0100

    Dev

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./app.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Services/>
+    </>
+  );
+}
+
+export default App;
